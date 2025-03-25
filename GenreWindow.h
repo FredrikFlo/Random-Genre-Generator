0d089bf101d5d270e84150848b04870824e26e1d
Fredrik Flo <flofredrik@gmail.com>
2025-03-25 11:01:19 +0100

    Add constexpr variables to GenreWindow.h

--- GenreWindow.h
+++ GenreWindow.h
@@ -1,6 +1,14 @@
 #include "AnimationWindow.h"
 
+constexpr int genreCellWidth;
+constexpr int genreCellHeight; 
+constexpr int ratingCellHeight; 
+constexpr int ratingCellHeight; 
+
+
 class genreWindow: public TDT4102::AnimationWindow
 {
-
+    private:
+    
+    public:
 };
