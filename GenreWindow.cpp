037c523136deba5e698b64396afbcde7f6f6c05f
Fredrik Flo <flofredrik@gmail.com>
2025-04-01 17:56:32 +0200

    Implementerte Rating Knapp

--- GenreWindow.cpp
+++ GenreWindow.cpp
@@ -68,26 +68,57 @@ void GenreWindow::UpdateDropDown()
     dropDownList.setOptions(genreVector.at(count)->sgNames);
 }
 
+void GenreWindow::RateCallback()
+{
+    int index = stringToCountMapVector.at(count).at(dropDownList.getSelectedValue());
+    try 
+    {
+        double newSGRating = std::stod(textInput.getText());
+        genreVector.at(count)->subGenres.at(index)->SetSubGenreRating(newSGRating);
+    }
+    catch(std::invalid_argument& e)
+    {
+        // Kanskje vise en error på skjermen
+    }
+    textInput.setText("");
+}
+
 GenreWindow::GenreWindow() : TDT4102::AnimationWindow{100, 100, windowWidth, windowHeight, "Random Genre Generator"}, 
-                             rateButton({650 + dropDownWidth/2 - buttonWidth/2, 600}, buttonWidth, buttonHeight, "RATE"),
+                             rateButton({630 + dropDownWidth/2 - buttonWidth/2, 600}, buttonWidth, buttonHeight, "RATE"),
                              leftButton({25, 200}, pageButtonWidth, pageButtonHeight, "<"),
                              rightButton({windowWidth-25-pageButtonWidth, 200}, pageButtonWidth, pageButtonHeight, ">"),
                              homeButton({25, 25}, 100, 100, "HOME"),
-                             dropDownList({650, 25}, dropDownWidth, dropDownHeight, vec)
+                             dropDownList({650, 25}, dropDownWidth, dropDownHeight, vec),
+                             textInput({930, 610}, 80, 30, "") 
 {
     add(rateButton);
     add(leftButton);
     add(rightButton);
     add(homeButton);
-    add(dropDownList);  // 4
+    add(dropDownList); 
+    add(textInput); 
 
-    //Images
     for (auto g : genreVector)
     {
+        //Lager en liste med bilder
         images.emplace_back(g->GetName() + ".jpg");
+
+        //Lager en vector av flere maps for indexen til hver instanse av SubGenre
+        std::map<std::string, int> tempMap;
+        int subGenrePlacement = 0;
+
+        for (auto sg : g->subGenres)
+        {
+            // map stuff
+            tempMap[sg->GetName()] = subGenrePlacement;
+            subGenrePlacement ++;
+        }
+
+        stringToCountMapVector.emplace_back(tempMap);
     }
 
     //Callbacks
     leftButton.setCallback(std::bind(&GenreWindow::DecrementCount, this));
     rightButton.setCallback(std::bind(&GenreWindow::IncrementCount, this));
+    rateButton.setCallback(std::bind(&GenreWindow::RateCallback, this));
 }
--- GenreWindow.h
+++ GenreWindow.h
@@ -6,8 +6,11 @@
 #include "FileHandling.h"
 #include "widgets/Button.h"
 #include "widgets/DropdownList.h"
+#include "widgets/TextInput.h"
 #include "Image.h"
 #include <format>
+#include <map>
+
 
 static constexpr int genreCellWidth = 350;
 static constexpr int ratingCellWidth = 100; 
@@ -20,6 +23,8 @@ static constexpr int pageButtonHeight = 300;
 static constexpr int pageButtonWidth = 75;
 static constexpr int dropDownWidth = 350;
 static constexpr int dropDownHeight = 75;
+static constexpr int textInputHeight = 100;
+static constexpr int textInputWidth = 250;
 static TDT4102::Point tableCorner = {150, 25};
 
 
@@ -32,18 +37,24 @@ class GenreWindow: public TDT4102::AnimationWindow
     TDT4102::Button rightButton;
     TDT4102::Button homeButton;
     TDT4102::DropdownList dropDownList;
+    TDT4102::TextInput textInput;
+    
     int count = 0;
+    std::vector<std::map<std::string, int>> stringToCountMapVector;
     
     public:
     const std::vector<std::shared_ptr<Genre>> genreVector = LoadFromFile("test.txt");
     std::vector<TDT4102::Image> images{};
     void DrawGenreCell(std::shared_ptr<Genre> genrePtr, const TDT4102::Point& position); //Bør funke for både en Genre og SubGenre pointer
     void DrawTable(); //Vil BARE funke for Genre pointer
-    void DecrementCount();
-    void IncrementCount();
     void DrawImage();
     void UpdateDropDown();
     int GetCount() const {return count;}
+    
+    //Call Back Funksjoner
+    void DecrementCount();
+    void IncrementCount();
+    void RateCallback(); 
     
     //Constructors
     GenreWindow();
